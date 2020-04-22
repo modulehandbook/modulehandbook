e039cc82dbaaf17e6711e2e6f02d45b51322c201
B Kleinen <kleinen@infrastructure.de>
2020-04-22 20:53:37 +0200

    created association in model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+  has_many :course_programs
+  has_many :programs, through: :course_programs
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,2 +1,4 @@
 class Program < ApplicationRecord
+  has_many :course_programs
+  has_many :courses, through: :course_programs
 end
