3fc6b8ffd9dd04ce52a0f3be048165ab624c7eaa
B Kleinen <kleinen@infrastructure.de>
2020-04-23 02:24:56 +0200

    bootstrap with webpack<

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'css/site'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery3")
-require("popper")
-require("bootstrap-sprockets")
-
+//require("jquery3")
+//require("popper")
+//require("bootstrap-sprockets")
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
