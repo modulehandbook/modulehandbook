5e023f4d94f924519ceece42c2042abbce7b44aa
B Kleinen <kleinen@infrastructure.de>
2020-04-23 18:43:09 +0200

    fixed tests

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  has_many :course_programs
+  has_many :course_programs, dependent: :destroy
   has_many :programs, through: :course_programs
   def select_name
     "#{name} (#{code})"
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,5 @@
 class Program < ApplicationRecord
-  has_many :course_programs
+  has_many :course_programs, dependent: :destroy
   has_many :courses, through: :course_programs
   def select_name
     "#{name} (#{code})"
