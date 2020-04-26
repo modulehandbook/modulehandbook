8f9dbdacf2824735176bfe416f28c805af03aef9
B Kleinen <kleinen@infrastructure.de>
2020-04-26 10:22:14 +0200

    added user approved - first steps

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    approved? ? super : :not_approved
+  end
 end
