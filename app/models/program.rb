9ec5cc4674f9855deaa1f82642a684bd8e6237fc
lotteunckell <41839717+lotteunckell@users.noreply.github.com>
2020-09-11 12:22:07 +0200

    Json Import & Export for Courses and Programs (#16)
    * rudimentary json exports done
    
    * added courses to program export
    
    * fixed export of all programs
    
    * added program names and codes to courses
    
    * renamed export links to have prefix export
    
    * fixed exported JSON and added import for one course
    
    * added course import to program#show with course-program-link
    
    * courses - allow multiple files and restrict to json
    
    * json import for program without courses
    
    * creating asociated courses and programs for imports
    
    * adding checks if course/program esists. Closes #13
    
    * wip
    
    * added TODOs for tests
    
    * added TODOs for tests 2
    
    * added simple tests for json import and export
    
    * bugfix and cleanup
    
    * added descriptions
    
    * incorporated answers to questions in #13
    
    * tryinig to fix pullrequest
    
    * tryinig to fix pullrequest 2

--- app/models/program.rb
+++ app/models/program.rb
@@ -4,4 +4,20 @@ class Program < ApplicationRecord
   def select_name
     "#{name} (#{code})"
   end
+
+  def self.create_from_json(data)
+    existing_program = Program.find_by(code: data['code'])
+    if !existing_program.nil?
+      program = existing_program
+    else
+      program = Program.new
+    end
+    program.name = data['name']
+    program.code = data['code']
+    program.mission = data['mission']
+    program.degree = data['degree']
+    program.ects = data['ects']
+    program.save
+    program
+  end
 end
