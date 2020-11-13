600d77ac180efbb9ae0b510df8df756039b8673e
lotteunckell <41839717+lotteunckell@users.noreply.github.com>
2020-11-13 01:12:30 +0100

    refactoring json imports, exports and tests (#18)

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -47,37 +47,4 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to courses_url
   end
-
-  test "should export a course as json" do
-    get export_course_json_url(id: @course.id)
-    assert_response :success
-    assert_includes response.body, @course.code
-    assert_includes response.body, @course.name
-  end
-
-  test "should export all courses as json" do
-    get export_courses_json_url
-    assert_response :success
-    assert_includes response.body, @course.code
-    assert_includes response.body, @course.name
-  end
-
-  test "should import a course from json" do
-    file = fixture_file_upload("#{Rails.root}/test/fixtures/files/2020-08-14_B1-Informatik1.json",'application/json')
-    files_array = Array.new
-    files_array << file
-    post import_course_json_url, params: { files: files_array }
-    assert_response :redirect
-    assert_redirected_to course_url(Course.find_by(code: 'B1'))
-  end
-
-  test "should import courses from multiple json" do
-    file1 = fixture_file_upload("#{Rails.root}/test/fixtures/files/2020-08-14_B1-Informatik1.json",'application/json')
-    file2 = fixture_file_upload("#{Rails.root}/test/fixtures/files/2020-08-14_B7-Informatik2.json",'application/json')
-    files_array = Array.new
-    files_array << file1 << file2
-    post import_course_json_url, params: { files: files_array }
-    assert_response :redirect
-    assert_redirected_to courses_url
-  end
 end
