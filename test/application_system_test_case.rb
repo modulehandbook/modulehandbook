fcdeafac2e5bee2af980090fd6fb4075b3cd760b
B Kleinen <kleinen@infrastructure.de>
2021-10-07 21:35:14 +0200

    wip

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,8 +3,8 @@ require 'capybara/poltergeist'
 require 'phantomjs'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  #driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   #driven_by :poltergeist, screen_size: [1400, 2800],
   #                        options: { js_errors: false,
   #                                   phantomjs: Phantomjs.path }
