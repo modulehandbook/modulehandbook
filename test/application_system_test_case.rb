245e1ec979926059ed59e18554bd384d5529162a
B Kleinen <kleinen@infrastructure.de>
2021-10-07 23:33:56 +0200

    remove save_and_open_page output and have it stored in tmp

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
-require 'capybara/poltergeist'
-require 'phantomjs'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  Capybara::save_path="./tmp"
   driven_by :selenium_headless #firefox
   # the comments tests dont work with chrome headless or poltergeist
   # driven_by :selenium_chrome_headless
