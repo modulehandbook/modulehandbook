1724a7ab02f746b76e41855164a21b4fe1dd2136
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-11-04 20:04:45 +0100

    config all environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.x.mh_hostname = ENV.fetch("HOSTNAME") { 'module-handbook.herokuapp.com' }
-  config.x.mh_devise_email = ENV.fetch("DEVISE_EMAIL") { 'module-handbook@infrastructure.de' }
-
   config.action_mailer.default_url_options = { host: config.x.mh_hostname }
   # Code is not reloaded between requests.
   config.cache_classes = true
