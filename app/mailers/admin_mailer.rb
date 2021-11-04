bc90a21ff8b0079fc1df7e3913f1dcddabc55527
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-11-04 19:38:05 +0100

    externalize host config

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,14 +1,13 @@
 
 class AdminMailer < Devise::Mailer
 
-  @@deviseEmail = ENV['DEVISE_EMAIL']
-  @@deviseEmail ||= 'module-handbook@infrastructure.de'
-
-  default from: @@deviseEmail
+  default from: Rails.configuration.x.mh_devise_email
   layout 'mailer'
 
   def new_user_waiting_for_approval(email)
     @email = email
-    mail(to: @@deviseEmail, subject: 'Module Handbook: New User Awaiting Admin Approval')
+    deviseEmail = Rails.configuration.x.mh_devise_email
+    @host = Rails.configuration.x.mh_hostname
+    mail(to: deviseEmail, subject: 'Module Handbook: New User Awaiting Admin Approval')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  host_name = ENV.fetch("HOSTNAME") { 'module-handbook.herokuapp.com' }
-  config.action_mailer.default_url_options = { host: host_name }
+
+  config.x.mh_hostname = ENV.fetch("HOSTNAME") { 'module-handbook.herokuapp.com' }
+  config.x.mh_devise_email = ENV.fetch("DEVISE_EMAIL") { 'module-handbook@infrastructure.de' }
+
+  config.action_mailer.default_url_options = { host: config.x.mh_hostname }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
