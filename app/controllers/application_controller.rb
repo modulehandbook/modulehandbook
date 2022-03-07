1eb33813b4f0889a5f73a5c171faa1aff5a93980
salim290 <salim.alkhodor@student.htw-berlin.de>
2022-03-07 17:14:42 +0100

    the header of course-program to arabic(2)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :authenticate_user!
+  def set_locale
+    I18n.locale = :ar
+  end
   check_authorization unless: :devise_controller?
 end
