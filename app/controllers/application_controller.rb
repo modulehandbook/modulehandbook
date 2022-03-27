71c526c9728528d361bf7d433c1fb9508c6bc136
salim290 <salim.alkhodor@student.htw-berlin.de>
2022-03-28 01:29:29 +0200

    switch between the languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!
+  private
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
   def set_locale
-    I18n.locale = :de
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ?
+      parsed_locale.to_sym :
+      nil
   end
   check_authorization unless: :devise_controller?
 end
