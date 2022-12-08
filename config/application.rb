cd8f40c9eca2970900894220369194de4845749d
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-12-08 01:37:49 +0100

    add all hostnames as the proxy sends them now

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module ModuleHandbook
 
     config.hosts << "module-handbook"
     config.hosts << "localhost"
+    config.hosts << "module-handbook.f4.htw-berlin.de"
+    config.hosts << "module-handbook-staging.f4.htw-berlin.de"
     # im nginx als schlichter 403 zu sehen:
     # [ActionDispatch::HostAuthorization::DefaultResponseApp] Blocked host: localhost
   end
