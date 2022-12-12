d7801e5c1f7f7f9dadfe367e1a1186150970bfd5
Youssef Henna <youssef.hisham14@gmail.com>
2022-12-12 13:56:48 +0100

    Add unique validation for course-program link + added case to skip SemesterDataAdder

--- app/models/course_program.rb
+++ app/models/course_program.rb
@@ -3,6 +3,7 @@ class CourseProgram < ApplicationRecord
   belongs_to :program, :foreign_key => [:program_id, :program_valid_end]
 
   validate :course_and_program_linkable
+  validate :unique_course_program_combination, on: :create
 
   def self.find_or_create_from_json(data, course_id, program_id)
     cpl = CourseProgram.find_by(course_id: course_id, program_id: program_id)
@@ -20,8 +21,20 @@ class CourseProgram < ApplicationRecord
 
   def course_and_program_linkable
     unless self[:course_valid_end] == self[:program_valid_end]
-      errors.add(:course_valid_end, message: "must match program_valid_end (same semester)")
-      errors.add(:program_valid_end, message: "must match course_valid_end (same semester)")
+      errors.add(:base, message: "Course and program must be in the same semester to be linked")
     end
+  end
+
+  def unique_course_program_combination
+    if self.class.exists?(
+      :course_id => self[:course_id],
+      :program_id => self[:program_id],
+      :course_valid_end => self[:course_valid_end],
+      :program_valid_end => self[:program_valid_end]
+    )
+
+      errors.add(:base, message: "Link with same course and program already exists")
+    end
+
   end
 end
