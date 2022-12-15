bd50acacc65a6c2264db6a7c520c56cef17fb47e
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-12-15 23:10:17 +0100

    add rescue for access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   check_authorization unless: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+      respond_to do |format|
+        format.json { head :forbidden }
+        format.html { redirect_to root_path, alert: exception.message }
+      end
+    end
 end
