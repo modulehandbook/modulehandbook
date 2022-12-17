46c1954d1602bfae08c4d7cff1367eaec4e46eea
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-12-18 00:00:36 +0100

    fix test

--- test/authorisation/abilities/writer/user_test.rb
+++ test/authorisation/abilities/writer/user_test.rb
@@ -5,6 +5,7 @@ require 'application_system_test_case'
 class UserWriterAbilitiesTest < ApplicationSystemTestCase
   setup do
     @user = users(:writer)
+    @user1 = users(:one)
     @user2 = users(:two)
     @ability = Ability.new(@user)
     system_test_login(@user.email, 'geheim12')
@@ -15,10 +16,10 @@ class UserWriterAbilitiesTest < ApplicationSystemTestCase
     assert @ability.cannot?(:new, @user2)
   end
 
-  test 'as writer i can read a user' do
-    assert @ability.can?(:read, @user2)
-    assert @ability.can?(:show, @user2)
-    assert @ability.can?(:index, @user2)
+  test 'as writer i can read a readable user' do
+    assert @ability.can?(:read, @user1)
+    assert @ability.can?(:show, @user1)
+    assert @ability.can?(:index, @user1)
   end
 
   test 'as writer i cant edit and update a user' do
