25abcab835d16f9cf3d24aeb2855e1db4815ad9c
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-12-17 09:07:49 +0100

    rails g scaffold faculty name:string url:string description:text

---
+++ db/migrate/20221217080356_create_faculties.rb
@@ -0,0 +1,11 @@
+class CreateFaculties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :faculties do |t|
+      t.string :name
+      t.string :url
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
