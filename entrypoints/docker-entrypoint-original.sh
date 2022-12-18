8714f61daad9bc0388bb68df0e3f3258161560de
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-12-18 02:24:59 +0100

    bump prod to 1.0.1

--- entrypoints/docker-entrypoint-original.sh
+++ entrypoints/docker-entrypoint-original.sh
@@ -9,8 +9,7 @@ fi
 echo "----------  RAILS_ENV = ${RAILS_ENV} --------"
 echo "----------  MODHAND_IMAGE = ${MODHAND_IMAGE} --------"
 
-bundle exec rails db:migrate
-bundle exec rake db:create_faculties
+
 bundle exec rails s -b 0.0.0.0
 
 # tail -f Gemfile
