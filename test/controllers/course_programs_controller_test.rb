350d57e8b1acb6bd168ed80a0b1e4abf68d69709
Youssef Henna <youssef.hisham14@gmail.com>
2023-01-04 11:27:16 +0100

    Added tests to controllers to accomdate versions

--- test/controllers/course_programs_controller_test.rb
+++ test/controllers/course_programs_controller_test.rb
@@ -12,6 +12,21 @@ class CourseProgramsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'should get index when provided as of time before the current time' do
+    get course_programs_url(params: { as_of_time: (Time.now - 1.hour).to_s })
+    assert_response :success
+  end
+
+  test 'should redirect to course_programs when "Reset" parameter' do
+    get course_programs_url(params: { commit: "Reset" })
+    assert_redirected_to course_programs_url
+  end
+
+  test 'should get versions' do
+    get course_program_versions_url(@course_program)
+    assert_response :success
+  end
+
   test 'should get new' do
     get new_course_program_url
     assert_response :success
@@ -33,6 +48,23 @@ class CourseProgramsControllerTest < ActionDispatch::IntegrationTest
   test 'should show course_program' do
     get course_program_url(@course_program)
     assert_response :success
+  end
+
+  # Sleeps for 2 seconds then sets as of time to 1 second ago
+  test 'should show course_program when provided as of time before the current time' do
+    sleep 2
+    get course_program_url(@course_program, params: { as_of_time: (Time.now - 1.second).to_s })
+    assert_response :success
+  end
+
+  test 'should redirect without as of time when course_program does not exist at provided as of time' do
+    get course_program_url(@course_program, params: { as_of_time: (Time.now - 40.year).to_s })
+    assert_redirected_to course_program_url(@course_program)
+  end
+
+  test 'should redirect to course_program when "Reset" parameter' do
+    get course_program_url(@course_program, params: { commit: "Reset" })
+    assert_redirected_to course_program_url(@course_program)
   end
 
   test 'should get edit' do
