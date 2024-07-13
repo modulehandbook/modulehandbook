6cc55c4c53db29a4d07923ed0c487b2c7614790a
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-07-13 18:45:58 +0200

    commit at 18:45:58

--- app/controllers/nudge_controller.rb
+++ app/controllers/nudge_controller.rb
@@ -6,12 +6,15 @@ class NudgeController < ApplicationController
     def nudge
         begin
           file_name = DateTime.now.strftime("nudge/nudge-%Y-%m-%d---%H-%M-%S")
-          @tag = params['tag']
-          File.open(file_name, 'w') { |file| file.write(@tag) }
-          @nudge_result = "ok - #{@tag} - #{file_name}"
+          tag = params['tag']
+          sha = params['sha']
+
+          File.open(file_name, 'w') { |file| file.write("tag: #{tag}\nsha: #{sha}) }
+          @nudge_result = "ok - #{@tag}/#{@sha} - #{file_name}"
         rescue Exception => e
             @nudge_result = e
         end
-
+#sha=9c17420beb4e38f5cdfc3a4f62fa3964de705d28
+#tag=sha-9c17420
     end
 end
