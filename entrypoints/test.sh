3f22fd7e505658a3065a9e3c63df5f197cf212c4
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-07-13 15:50:01 +0200

    add modhand-test target to Dockerfile

---
+++ entrypoints/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+echo "----------  RAILS_ENV = ${RAILS_ENV} --------"
+echo "----------  MODHAND_IMAGE = ${MODHAND_IMAGE} --------"
+
+bin/rails rails db:migrate
+bin/rails rake db:create_faculties
+bin/rails test
