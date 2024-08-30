97f90cd7ba1880e51d0aa274b6faba23a5111ad2
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-08-30 22:31:27 +0200

    check for credentials in config

--- config/environment.rb
+++ config/environment.rb
@@ -5,16 +5,17 @@ require_relative 'application'
 
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
-# Mailer configuration for MailGun on Heroku.
-ActionMailer::Base.smtp_settings = {
-  port: Rails.application.credentials.devise.smtp_port,
-  address: Rails.application.credentials.devise.smtp_server,
-  user_name: Rails.application.credentials.devise.smtp_login,
-  password: Rails.application.credentials.devise.smtp_password,
-  authentication: :login,
-  enable_starttls_auto: true
-  #:domain         => 'module-handbook.heroku.com',
-}
+
+unless Rails.application.credentials.devise.nil?
+  ActionMailer::Base.smtp_settings = {
+    port: Rails.application.credentials.devise.smtp_port,
+    address: Rails.application.credentials.devise.smtp_server,
+    user_name: Rails.application.credentials.devise.smtp_login,
+    password: Rails.application.credentials.devise.smtp_password,
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
