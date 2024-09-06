01f9292530c2364c31e1c74b586e65cac71c009d
flashwing-04 <charlotte.john.04@gmx.de>
2024-09-06 19:36:44 +0200

    remove unessecary method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,8 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   
-  after_action :handle_unconfirmed_user, only: [:create]
-  
   # GET /resource/sign_up
   # def new
   #   super
@@ -61,17 +59,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-
-  private
-
-  def handle_unconfirmed_user
-    if resource.persisted? && !resource.confirmed?
-      set_flash_message :notice, :signed_up_but_unconfirmed
-    elsif resource.persisted? && !resource.approved?
-      set_flash_message :notice, :signed_up_but_not_approved
-    end
-    sign_out(resource)
-    return 
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
