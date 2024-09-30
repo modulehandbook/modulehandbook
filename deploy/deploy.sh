1093189dfff21119a13c88705ccd4e94a5107a4a
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-09-30 15:30:46 +0200

    use staging action to deploy staging

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,12 +13,12 @@ scp Makefile.prod $HOST:~/Makefile
 scp compose.yaml $HOST:~
 scp -r nginx $HOST:~
 scp -r deploy $HOST:~
-ssh $HOST "ln -s deploy/environments/$ENV.env active-environment.env"
-echo RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key) | ssh $HOST "cat >> .env"
+ssh $HOST "ln -sf deploy/environments/$ENV.env active-environment.env"
 
-
+# .env
 scp .env $HOST:~
 ssh $HOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
+echo RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key) | ssh $HOST "cat >> .env"
 
 ssh $HOST "sudo docker compose down"
 ssh $HOST "sudo docker compose up -d"
