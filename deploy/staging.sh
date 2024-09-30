d4c32bad88baa4973ed607e4e37ab0421f956c31
annadeus <anna.groepler@gmail.com>
2024-09-30 14:39:12 +0200

    push image from branch

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ENV=staging
-TAG=sha-$(git rev-parse --short origin/main)
+#TAG=sha-$(git rev-parse --short origin/main)
+TAG=sha-$(git rev-parse --short origin/new_deployment_scrapbook)
 
 deploy/deploy.sh $ENV $TAG
 
