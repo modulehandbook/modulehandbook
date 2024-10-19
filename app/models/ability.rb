e064b88856b04bb18ba244b29b82c5714996deea
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-10-19 22:03:45 +0200

    ability: green

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,38 +8,13 @@ class Ability
   def initialize(user)
     aliases
 
-    #not_logged_in_abilities(user)
+    not_logged_in_abilities(user)
     return if user.blank?
 
     raise("non-existent user role #{user.role}") unless User::ROLES.include?(user.role.to_sym)
 
-    User::ROLES.each do | user_role |# ROLES = %i[reader writer editor qa admin].freeze
-      method_name = "#{user_role}_abilities"
-      self.send(method_name, user)
-      return if user.role == user_role
-    end
-      merge Abilities::Reader.new(user)
-    return if user.role == 'reader'
-
-    # return unless user.role == 'writer' || user.role == 'editor' || user.role == 'qa' || user.role == 'admin'
-
-    merge Abilities::Writer.new(user)
-    return if user.role == 'writer'
-
-    # return unless user.role == 'editor' || user.role == 'qa' || user.role == 'admin'
-    role = 'editor'
-    method_name = "#{role}_abilities"
+     method_name = "#{user.role}_abilities"
     self.send(method_name, user)
-    #editor_abilities(user)
-    #merge Abilities::Editor.new(user)
-    return if user.role == 'editor'
-
-    # return unless user.role == 'qa' || user.role == 'admin'
-
-    merge Abilities::Qa.new(user)
-    return unless user.role == 'admin'
-
-    merge Abilities::Admin.new(user)
   end
 
   def aliases
@@ -52,6 +27,53 @@ class Ability
     alias_action :export_program_json, :export_programs_json, :export_program_docx, to: :export_program
     alias_action :import_course_json, to: :import_course
     alias_action :import_program_json, to: :import_program
+  end
+
+  def not_logged_in_abilities(user);end
+  def reader_abilities(user)
+    not_logged_in_abilities(user)
+    can %i[read update], User, id: user.id
+    can %i[read], User, readable: true
+    can %i[read export_course], Course
+    can %i[read export_program], Program
+    can %i[read], Faculty
+    can %i[read], CourseProgram
+    reader_comment_abilities(user)
+  end
+
+
+  def reader_comment_abilities(user)
+    can %i[read], Comment
+    can %i[create], Comment
+    can %i[destroy], Comment, author_id: user.id
+    can %i[edit update], Comment, Comment do |comment|
+      comment.author_id == user.id && comment.created_at >= 30.minutes.ago
+    end
+  end
+
+
+  def writer_abilities(user)
+    reader_abilities(user)
+    can %i[crud], CourseProgram
+    can %i[crud export_course import_course versions], Course
+    can %i[crud export_program import_program], Program
+  end
+
+  def editor_abilities(user)
+    writer_abilities(user)
+    can %i[crud], CourseProgram
+    can %i[create read update], Faculty
+    can %i[crud export_course import_course change_state revert_to], Course
+    can %i[crud export_program import_program], Program
+    can %i[read approve], User
+  end
+  def qa_abilities(user)
+    editor_abilities(user)
+  end
+
+  def admin_abilities(user)
+    qa_abilities(user)
+    can :manage, :all
   end
 
   def self.list_actions_by_module
