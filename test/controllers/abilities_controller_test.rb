c21b18560ed4d62d482180d8f0d5547c5e06f7e7
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-10-19 22:25:06 +0200

    add show abilities test

--- test/controllers/abilities_controller_test.rb
+++ test/controllers/abilities_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class AbilitiesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = users(:one)
+    @qa = users(:qa)
+    sign_in @admin
+
+  end
+  test 'show index / abilities overview' do
+    get abilities_path
+    assert_response :success
+  end
 end
