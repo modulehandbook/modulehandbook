58e9cf0386fdc74185139e88972330f4dc1b076e
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-10-30 18:47:15 +0100

    add mh-imi host -> needs to go in config

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module ModuleHandbook
     config.hosts << 'module-handbook.f4.htw-berlin.de'
     config.hosts << 'module-handbook-staging.f4.htw-berlin.de'
     config.hosts << 'makesign.f4.htw-berlin.de'
+    config.hosts << 'mh-imi.f4.htw-berlin.de'
+    
     # config.hosts << "141.45.191.40"
     # config.hosts << "141.45.191.41"
 
