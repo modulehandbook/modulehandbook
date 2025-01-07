ab1f5d0312a2dc11713479050de03b47547c25c6
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-01-07 13:37:41 +0100

    introduce rubocop error to test action

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
 
-# Topics describe cross-cutting topics
-# for a program. This model holds the
-# normalized Title for Topics
 class Topic < ApplicationRecord
   has_paper_trail
   has_many :topic_descriptions, dependent: :destroy
