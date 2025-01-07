4d3bf302cde607de44bf5c11c0e2fd4475f1248a
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-01-07 13:39:55 +0100

    Revert "introduce rubocop error to test action"
    This reverts commit ab1f5d0312a2dc11713479050de03b47547c25c6.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
 
+# Topics describe cross-cutting topics
+# for a program. This model holds the
+# normalized Title for Topics
 class Topic < ApplicationRecord
   has_paper_trail
   has_many :topic_descriptions, dependent: :destroy
