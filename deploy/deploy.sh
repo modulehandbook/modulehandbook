e75007cee347856f6a74a0824a1e0fde903508ab
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-01-09 20:44:26 +0100

    Automatic deployment - first version (#212)
    * automatic deployment staging 01
    
    * automatic deployment staging 02
    
    * automatic deployment staging 03
    
    * automatic deployment staging - try fix handling master-key
    
    * automatic deployment staging - change triggering branch to main
    
    * automatic deployment staging - change trigger to workflow build and push image
    
    * integrate staging deployment into structure workflow
    
    * add deploy production workflow
    
    * try fix handling secrets in workflow call
    
    * fix secret handling for staging master key
    
    * fix secret handling for production and disable workflow_call to deploy if input push false
    
    * add ssh test file
    
    * little changes in deploy.sh, first try at gitops in one file
    
    * test ssh
    
    * disable deploy-imi-production
    
    ---------
    
    Co-authored-by: flashwing-04 <charlotte.john.04@gmx.de>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,8 @@
 ENV=$1
 TAG=$2
 
+RAILS_MASTER_KEY=$RAILS_MASTER_KEY
+
 source ./deploy/environments/$ENV.env
 echo "about to deploy commit $TAG to $ENV"
 
@@ -11,18 +13,21 @@ echo "-----------"
 scp Makefile.prod $HOST:~/Makefile
 scp compose.yaml $HOST:~
 scp -r nginx $HOST:~
-scp -r deploy $HOST:~
-ssh $HOST "ln -sf deploy/environments/$ENV.env active-environment.env"
+scp deploy/environments/$ENV.env $HOST:active-environment.env
 
-# .env
-scp .env $HOST:~
-ssh $HOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
-RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key)
+# RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key)
 if [ $RAILS_MASTER_KEY = "" ]; then
   echo "RAILS_MASTER_KEY missing"
   exit 42
 fi
+
+# .env
+
+ssh $HOST "echo TAG_MODULE_HANDBOOK_EXPORTER=sha-4ef1b2d > .env"
 ssh $HOST "echo RAILS_MASTER_KEY=$RAILS_MASTER_KEY >> .env"
+ssh $HOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
+
+
 #echo RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key) | ssh $HOST "cat >> .env"
 
 ssh $HOST "sudo docker compose down"
