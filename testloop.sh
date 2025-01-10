914458080ccdda606008847d51977045c8c48f26
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-01-10 19:44:04 +0100

    green

--- testloop.sh
+++ testloop.sh
@@ -6,9 +6,14 @@ while true; do
     if [[ "$1" == "system" ]]
     then
       bin/rails test:system
+    elif [[ "$1" == "lint" ]]
+    then
+      make static_code_checks
     else
       bin/rails test
     fi
+
+
 
     fswatch -1 ./**/*.rb ./**/*.yml ./**/*.html.erb
 done
