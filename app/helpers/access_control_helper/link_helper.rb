47f78fc756b88277b1053dfc0baee2b7fbf1a997
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-01-10 02:48:07 +0100

    create MockContext for test

--- app/helpers/access_control_helper/link_helper.rb
+++ app/helpers/access_control_helper/link_helper.rb
@@ -3,10 +3,29 @@
 module AccessControlHelper
     module LinkHelper
 
-        def my_can?(*args)
-            can?(*args)
+        # todo: dry with method missing, see
+        # https://stackoverflow.com/questions/49712163/ruby-module-that-delegates-methods-to-an-object
+        def link_to_delete(link_text, resource, confirmation = 'Are you sure?')
+            LinkGenerator.new(self).link_to_delete(link_text, resource, confirmation)
         end
 
+        def link_to_edit(link_text, resource, options = {})
+            LinkGenerator.new(self).link_to_edit(link_text, resource, options)
+        end
+
+        def link_to_new(link_text, clazz, path_args = {})
+            LinkGenerator.new(self).link_to_new(link_text, clazz, path_args)
+        end
+
+
+        class LinkGenerator
+            def initialize(controller_context)
+                @controller_context = controller_context
+            end
+
+        def my_can?(*args)
+            @controller_context.can?(*args)
+        end
 
         def options_hash(opts)
             opts_with_default = Hash.new('')
@@ -22,8 +41,8 @@ module AccessControlHelper
           if my_can? :edit,resource
               snake = snake(resource.class)
               path_helper = "edit_#{snake}_path"
-              path = self.send(path_helper, resource)
-              link = link_to(link_text, path, id: "edit_#{snake}_#{resource.id}")
+              path = @controller_context.send(path_helper, resource)
+              link = @controller_context.link_to(link_text, path, id: "edit_#{snake}_#{resource.id}")
               link+opt[:suffix]
               #"#{link}#{opt[:suffix]}"
           else
@@ -36,8 +55,8 @@ module AccessControlHelper
             if my_can? :create, clazz
                 snake = snake(clazz)
                 path_helper = "new_#{snake}_path"
-                path = self.send(path_helper, path_args)
-                link_to(link_text, path, id: "new_#{snake}")
+                path = @controller_context.send(path_helper, path_args)
+                @controller_context.link_to(link_text, path, id: "new_#{snake}")
             else
                 "<!-- new link omitted -->"
             end
@@ -46,13 +65,12 @@ module AccessControlHelper
         def link_to_delete(link_text, resource, confirmation = 'Are you sure?')
             if my_can? :destroy, resource
                 snake = snake(resource.class)
-                link_to(link_text, resource, id: "delete_#{snake}_#{resource.id}", data: { turbo_method: :delete, turbo_confirm: confirmation })
+                @controller_context.link_to(link_text, resource, id: "delete_#{snake}_#{resource.id}", data: { turbo_method: :delete, turbo_confirm: confirmation })
             else
                 "<!-- delete link omitted -->"
             end
         end
-             
-
+    end
 
     end
 end
--- test/helpers/access_control_helper/link_helper_test.rb
+++ test/helpers/access_control_helper/link_helper_test.rb
@@ -2,22 +2,44 @@
 
 require 'test_helper'
 
-module AccessControlHelper
-  module LinkHelper
-    def my_can?(*args)
-      user = users(:one)
-      ability = Ability.new(user)
-      ability.can?(*args)
-    end
+# The LinkHelper uses cancancan's can? method
+# which is not available in ActionView::TestCase
+# this MockContext implements the can? method
+# and delegates everything else to the current
+# test class/object.
+# todo: can made more versatile by passing the user object
+# as a parameter
+
+class MockContext
+  attr_accessor :action_view_test
+  def initialize(action_view_test)
+    self.action_view_test = action_view_test
   end
+
+ def can?(*args)
+    user = users(:one)
+    ability = Ability.new(user)
+    ability.can?(*args)
+ end
+
+
+  def method_missing(m, *args, &block)
+    #puts "Delegating #{m}"
+    action_view_test.send(m, *args, &block)
+  end
+
 end
 
 include AccessControlHelper::LinkHelper
 
 class LinkHelperTest < ActionView::TestCase
+
+  setup do
+    @link_generator = LinkGenerator.new(MockContext.new(self))
+  end
   test 'link_to_edit' do
     course = courses(:one)
-    link = link_to_edit('Edit Label', course, suffix: ' danach')
+    link = @link_generator.link_to_edit('Edit Label', course, suffix: ' danach')
     assert_dom_equal %{<a id="edit_course_#{course.id}" href="/courses/#{course.id}/edit">Edit Label</a> danach}, link
   end
 
