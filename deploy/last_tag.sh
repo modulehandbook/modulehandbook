1813b79528623a6edc803cddb6a132bea06faa5b
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-05-16 20:09:11 +0200

    ..

---
+++ deploy/last_tag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TAG=$(git -c 'versionsort.suffix=-' \
+    ls-remote --exit-code --refs --sort='version:refname' --tags origin '*.*.*' \
+    | tail --lines=1 \
+    | cut -d '/' -f 3)
+#    | cut --delimiter='/' --fields=3
+
+echo $TAG
--- deploy/production.sh
+++ deploy/production.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 ENV=$1
-TAG=$(git -c 'versionsort.suffix=-' \
-    ls-remote --exit-code --refs --sort='version:refname' --tags origin '*.*.*' \
-    | tail --lines=1 \
-    | cut -d '/' -f 3)
-#    | cut --delimiter='/' --fields=3
-
+TAG=$2
 
 deploy/deploy.sh $ENV $TAG
-
