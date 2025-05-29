9d12041659ffdbee242159ae01cdab0d4b9a3514
flashwing-04 <charlotte.john.04@gmx.de>
2025-05-29 14:56:05 +0200

    enable host authorization in staging.rb/ production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.hosts << "modulehandbook_giu"
+  config.hosts << "module-handbook-staging.f4.htw-berlin.de"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -98,6 +98,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "modulehandbook_stag"
+  config.hosts << "module-handbook-staging.f4.htw-berlin.de"
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
