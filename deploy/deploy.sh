e5a6d8dadd7a612641f90f982e1ede2a2646a892
flashwing-04 <charlotte.john.04@gmx.de>
2025-05-29 13:22:49 +0200

    adjust deploy script and temporarily have branch push docker image

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,22 +10,43 @@ echo "about to deploy commit $TAG to $ENV"
 set -x
 echo "-----------"
 
-scp Makefile.prod $HOST:~/Makefile
-scp compose.yaml $HOST:~
-scp -r nginx $HOST:~
-scp deploy/environments/$ENV.env $HOST:active-environment.env
+if [ "$ENV" = "staging" ]; then
+  COMPOSE_FILE="compose.staging.yaml"
+  NGINX_FOLDER="nginx_staging"
 
-# RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key)
-if [ $RAILS_MASTER_KEY = "" ]; then
-  echo "RAILS_MASTER_KEY missing"
-  exit 42
+  ssh $HOST "mkdir -p ~/deploy/environments"
+  scp deploy/environments/staging.env $HOST:deploy/environments/staging.env
+  scp deploy/environments/giu_production.env $HOST:deploy/environments/giu_production.env
+else
+  COMPOSE_FILE="compose.yaml"
+  NGINX_FOLDER="nginx"
+
+  scp deploy/environments/$ENV.env $HOST:active-environment.env
+fi
+
+scp Makefile.prod $HOST:~/Makefile
+scp $COMPOSE_FILE $HOST:~/compose.yaml
+scp -r $NGINX_FOLDER $HOST:nginx
+
+if [ "$ENV" != "staging" ]; then
+  RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key 2>/dev/null)
+  if [ -z "$RAILS_MASTER_KEY" ]; then
+    echo "RAILS_MASTER_KEY missing for $ENV"
+    exit 42
+  fi
 fi
 
 # .env
 
 ssh $HOST "echo TAG_MODULE_HANDBOOK_EXPORTER=sha-4ef1b2d > .env"
-ssh $HOST "echo RAILS_MASTER_KEY=$RAILS_MASTER_KEY >> .env"
 ssh $HOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
+
+if [ "$ENV" = "staging" ]; then
+  ssh $HOST "echo RAILS_MASTER_KEY_STAGING=$RAILS_MASTER_KEY_STAGING >> .env"
+  ssh $HOST "echo RAILS_MASTER_KEY_PRODUCTION=$RAILS_MASTER_KEY_PRODUCTION >> .env"
+else
+  ssh $HOST "echo RAILS_MASTER_KEY=$RAILS_MASTER_KEY >> .env"
+fi
 
 
 #echo RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key) | ssh $HOST "cat >> .env"
