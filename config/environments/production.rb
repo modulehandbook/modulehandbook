a89184c7e66169ff1bc178d27178e9d43d893c2c
flashwing-04 <charlotte.john.04@gmx.de>
2025-06-04 15:30:18 +0200

    add localhost to host config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,8 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
+  config.hosts << "localhost"
 
-  config.hosts << "modulehandbook_giu"
-  config.hosts << "module-handbook-staging.f4.htw-berlin.de"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
