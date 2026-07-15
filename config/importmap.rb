5dedf618677d5750e65ef87e4042b46b5e1abe59
Barne Kleinen <barne.kleinen@htw-berlin.de>
2026-07-15 21:20:00 +0200

    ./bin/rails css:install:bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@
 pin 'application', preload: true
 pin 'bootstrap', preload: true # @5.3.3
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin "bootstrap", to: "bootstrap.bundle.min.js"
