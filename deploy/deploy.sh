f9669f2aec073fb1f0b7a6fe151ab8010694d517
Barne Kleinen <barne.kleinen@htw-berlin.de>
2026-07-16 12:45:15 +0200

    remove sudo from compose commands

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -30,5 +30,5 @@ ssh $HOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
 
 #echo RAILS_MASTER_KEY=$(cat secrets/config/credentials/$ENV.key) | ssh $HOST "cat >> .env"
 
-ssh $HOST "sudo docker compose down"
-ssh $HOST "sudo docker compose up -d"
+ssh $HOST "docker compose down"
+ssh $HOST "docker compose up -d"
