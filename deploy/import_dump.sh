9ffe0dfe505a85eefda91a68ba591ca11ea95495
Barne Kleinen <barne.kleinen@htw-berlin.de>
2026-07-16 17:45:00 +0200

    fix import dump

--- deploy/import_dump.sh
+++ deploy/import_dump.sh
@@ -7,4 +7,4 @@ DF=dump-for-import.pgdump
 
 #scp $DUMP_FILE $HOST:$DF
 
-cat $(DUMP_FILE) | ssh local@module-handbook-staging.f4.htw-berlin.de "docker compose exec -T module-handbook-postgres pg_restore --verbose --clean --no-acl --no-owner -h localhost -U modhand -d modhand-db-prod"
+cat ${DUMP_FILE} | ssh local@module-handbook-staging.f4.htw-berlin.de "docker compose exec -T module-handbook-postgres pg_restore --verbose --clean --no-acl --no-owner -h localhost -U modhand -d modhand-db-prod"
