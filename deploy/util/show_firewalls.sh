fe994bb4029990ac346c738369edaba81bed5391
Barne Kleinen <barne.kleinen@htw-berlin.de>
2026-07-18 13:48:47 +0200

    add firewall script

---
+++ deploy/util/show_firewalls.sh
@@ -0,0 +1,11 @@
+#
+
+tt=$(date +%Y-%m-%d--%H-%M-%S)
+
+for h in   module-handbook-staging.f4.htw-berlin.de mh-imi.f4.htw-berlin.de module-handbook.f4.htw-berlin.de
+do
+    echo 
+    echo "-----------   $h   -----------"
+    echo
+    ssh local@$h "sudo iptables -L -n -v --line-numbers" > deploy/util/iptables/$h-$tt.txt
+done
--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -9,12 +9,17 @@ fi
 echo "----------  RAILS_ENV = ${RAILS_ENV} --------"
 echo "----------  MODHAND_IMAGE = ${MODHAND_IMAGE} --------"
 
-if [ "$RAILS_ENV" == "development" ]; then
-  echo "environment is dev, skipping asset precompilation"
-  bundle exec rails css:build
-else
-  bundle exec rails assets:precompile
-fi
+# 18.07.26: das war ein Versuch assets precompile aus dem 
+# Dockerfile rauszunehmen und stattdessen beim Neustart 
+# auszuführen. 
+# der Container crasht aber bei rails assets:precompile.
+# 
+# if [ "$RAILS_ENV" == "development" ]; then
+#   echo "environment is dev, skipping asset precompilation"
+#   bundle exec rails css:build
+# else
+#   bundle exec rails assets:precompile
+# fi
 
 bundle exec rails db:migrate
 bundle exec rails s -b 0.0.0.0
