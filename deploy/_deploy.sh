dd96cf50500ea6aff5161223b977145cb7ed3e82
Barne Kleinen <barne.kleinen@htw-berlin.de>
2026-07-19 13:33:35 +0200

    quickpush 13-33-35 workflow wip

--- deploy/_deploy.sh
+++ deploy/_deploy.sh
@@ -10,8 +10,22 @@
 ENV=$1
 TAG=$2
 
+if [[ "$ENV" == "" || "$TAG" == "" ]; then
+  echo usage: _deploy <ENV> <TAG> 
+  exit 1
+fi
+
+if [[ "$USER" == "" ]; then
+  echo $USER environment variable needs to be set 
+  exit 2
+fi
+
+
 source ./deploy/environments/$ENV.env
 echo "---  deploy $TAG to $ENV ($HOST)"
+
+UHOST="$USER@$HOST"
+
 
 if [ "$RAILS_MASTER_KEY" == "" ]; then
   echo "RAILS_MASTER_KEY not set, using config/credentials/$ENV.key"
@@ -28,24 +42,24 @@ fi
 set -x
 echo "-----------"
 
-scp compose.yaml $HOST:~
+scp compose.yaml $UHOST:~
 
-scp -r nginx $HOST:~
-scp -r entrypoints $HOST:~
+scp -r nginx $UHOST:~
+scp -r entrypoints $UHOST:~
 
 
-scp deploy/vms/copy_to_host_home_directories/* $HOST:~
-scp deploy/vms/copy_to_host_home_directories/.bashrc $HOST:~
+scp deploy/vms/copy_to_host_home_directories/* $UHOST:~
+scp deploy/vms/copy_to_host_home_directories/.bashrc $UHOST:~
 
 
 # .env
-scp ./deploy/environments/$ENV.env $HOST:~/.env
+scp ./deploy/environments/$ENV.env $UHOST:~/.env
 
-ssh $HOST "echo TAG_MODULE_HANDBOOK_EXPORTER=sha-4ef1b2d >> .env"
-ssh $HOST "echo RAILS_MASTER_KEY=$RAILS_MASTER_KEY >> .env"
-ssh $HOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
+ssh $UHOST "echo TAG_MODULE_HANDBOOK_EXPORTER=sha-4ef1b2d >> .env"
+ssh $UHOST "echo RAILS_MASTER_KEY=$RAILS_MASTER_KEY >> .env"
+ssh $UHOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
 
 
 
-ssh $HOST "docker compose down"
-ssh $HOST "docker compose up -d"
+ssh $UHOST "docker compose down"
+ssh $UHOST "docker compose up -d"
