a68c04b018ef61a806e4b526c22b55f07306bb0e
Barne Kleinen <barne.kleinen@htw-berlin.de>
2026-07-20 15:55:13 +0200

    cleanup

--- deploy/_deploy.sh
+++ deploy/_deploy.sh
@@ -59,7 +59,8 @@ ssh $UHOST "echo TAG_MODULE_HANDBOOK_EXPORTER=sha-4ef1b2d >> .env"
 ssh $UHOST "echo RAILS_MASTER_KEY=$RAILS_MASTER_KEY >> .env"
 ssh $UHOST "echo TAG_MODULE_HANDBOOK=$TAG >> .env"
 
-ssh $UHOST "./docker-cleanup.sh"
+
 
 ssh $UHOST "docker compose down"
+ssh $UHOST "./docker-cleanup.sh"
 ssh $UHOST "docker compose up -d"
